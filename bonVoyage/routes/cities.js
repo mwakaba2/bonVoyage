88bb3fa1f1fbcd5a414fb10ff63214b0169bc471
Mariko Wakabayashi <marimarimekko@gmail.com>
2015-10-04 16:59:17 -0500

    CRUD for cities

---
+++ bonVoyage/routes/cities.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/bonVoyage');
+
+router.get('/', function(req, res) {
+    var collection = db.get('cities');
+    collection.find({}, function(err,cities){
+        if (err) throw err;
+      	res.json(cities);
+    });
+});
+
+router.post('/', function(req, res){
+    var collection = db.get('cities');
+    collection.insert({
+        name: req.body.name,
+        country: req.body.country
+    }, function(err, city){
+        if (err) throw err;
+
+        res.json(city);
+    });
+});
+
+
+router.get('/:id', function(req, res) {
+    var collection = db.get('cities');
+    collection.findOne({ _id: req.params.id }, function(err, city){
+        if (err) throw err;
+
+      	res.json(city);
+    });
+});
+
+router.put('/:id', function(req, res){
+    var collection = db.get('cities');
+    collection.update({
+        _id: req.params.id
+    },
+    {
+        name: req.body.name,
+        country: req.body.country
+    }, function(err, city){
+        if (err) throw err;
+
+        res.json(city);
+    });
+});
+
+
+router.delete('/:id', function(req, res) {
+    var collection = db.get('cities');
+    collection.remove({ _id: req.params.id }, function(err, city){
+        if (err) throw err;
+
+      	res.json(city);
+    });
+});
+
+
+module.exports = router;
---
+++ bonVoyage/routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/', function(req, res) {
+    var db = req.db;
+    var collection = db.get('users');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('users');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('users');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+module.exports = router;
