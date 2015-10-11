e5c170d223cf02dc6099178ec5135769f6146873
Mariko Wakabayashi <marimarimekko@gmail.com>
2015-10-10 21:52:36 -0500

    Created user authentication with UserApp API

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('bonVoyage', ['ngResource', 'ngRoute']);
+var app = angular.module('bonVoyage', ['ngResource', 'ngRoute', 'UserApp']);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
@@ -14,9 +14,23 @@ app.config(['$routeProvider', function ($routeProvider) {
             controller: 'CityCtrl'
         })
         .when('/signup', {
-            templateUrl: 'templates/signup.html'
+            templateUrl: 'templates/signup.html',
+            public: true,
+        })
+        .when('/login', {
+            templateUrl: 'templates/login.html',
+            public: true,
+            login: true
+        })
+        .when('/user', {
+            templateUrl: 'templates/user.html',
+            controller: 'UserCtrl'
         })
         .otherwise({
             redirectTo: '/'
         });
 }]);
+
+app.run(function($rootScope, user) {
+    user.init({ appId: '5619bd59cca34' });
+});
