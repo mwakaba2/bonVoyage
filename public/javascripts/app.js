18e17448d19e8f8a05f518ddf4e40f4b7e2a1113
Mariko Wakabayashi <marimarimekko@gmail.com>
2015-10-10 22:00:22 -0500

    Made current pages public to users and non users

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,13 +3,16 @@ var app = angular.module('bonVoyage', ['ngResource', 'ngRoute', 'UserApp']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
+            public: true,
             redirectTo: '/cities'
         })
         .when('/cities', {
+            public: true,
             templateUrl: 'templates/cities.html',
             controller: 'CitiesCtrl'
         })
         .when('/city/:id', {
+            public: true,
             templateUrl: 'templates/city.html',
             controller: 'CityCtrl'
         })
@@ -19,14 +22,14 @@ app.config(['$routeProvider', function ($routeProvider) {
         })
         .when('/login', {
             templateUrl: 'templates/login.html',
-            public: true,
-            login: true
+            public: true
         })
         .when('/user', {
             templateUrl: 'templates/user.html',
             controller: 'UserCtrl'
         })
         .otherwise({
+            public: true,
             redirectTo: '/'
         });
 }]);
