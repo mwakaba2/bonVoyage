5a4acc3002e614927fd36716caf92ab68d53bf72
hwang381 <hwang381@gatech.edu>
2015-10-30 12:06:53 -0500

    Import... (WIP)

---
+++ bvData/imports/config.py
@@ -0,0 +1,15 @@
+local = {
+    host: "localhost"
+    db_anme: "bonVoyage"
+    collection: "cities"
+    username: ""
+    password: ""
+}
+
+heroku = {
+    host: "ds033018.mongolab.com:33018"
+    db_anme: "heroku_358800q5"
+    collection: "cities"
+    username: "bv"
+    password: "bonvoyage2015"
+}
---
+++ bvData/imports/data_import_heroku.sh
@@ -0,0 +1,1 @@
+mongoimport -h ds033018.mongolab.com:33018 -d heroku_358800q5 -c cities -u bv -p bonvoyage2015 --file data.json --jsonArray
