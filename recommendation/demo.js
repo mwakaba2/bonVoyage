3346b223d5860fa4ba0bdd34ead7e86ef52736ba
hwang381 <hwang381@gatech.edu>
2015-11-13 11:10:23 -0600

    Fix small bug in recommendation

--- recommendation/demo.js
+++ recommendation/demo.js
@@ -1,12 +1,12 @@
 var recommend = require('./recommendation');
 
 var bookmarked_cities = ['Paris', 'Berlin'];
-var viewed_cities = ['Lhasa'];
+var viewed_cities = [];
 
 var monk = require('monk');
 var db = monk('localhost:27017/bonVoyage');
 db.get('cities').find({}, function (err, cities) {
     if (err) throw err;
 
-    console.log(recommend(cities, bookmarked_cities, viewed_cities));
+    console.log(recommend(cities, bookmarked_cities, viewed_cities).slice(0, 10));
 });
